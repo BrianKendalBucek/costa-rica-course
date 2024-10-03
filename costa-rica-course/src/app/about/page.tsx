3d4670d036c67d1e360b2d3ff7ac378daabe4407
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2024-10-03 10:06:47 -0600

    header complete

--- costa-rica-course/src/app/about/page.tsx
+++ costa-rica-course/src/app/about/page.tsx
@@ -6,8 +6,13 @@ import styles from "./about.module.scss"; // Create a corresponding module.scss 
 const AboutPage = () => {
   return (
     <div className={styles.aboutContainer}>
-      <div className={styles.hero}>Test</div>
-      <h1 className={styles.title}>About Us</h1>
+      <div className={styles.hero}>
+        <div className={styles.heroTitle}>
+          <h1 className={styles.heroTitleOne}>ABOUT</h1>
+          <h1 className={styles.heroTitleTwo}>US</h1>
+        </div>
+        <p>Our team, a blend of experienced expats and native Costa Ricans, is dedicated to guiding you through every step of your relocation journey with expertise and passion.</p>
+      </div>
       <p className={styles.description}>
         Welcome to our website! We are passionate about providing you with the best experience possible. 
         Our mission is to guide expats and travelers through their journey in Costa Rica by providing 
