b664be36a90091f0eed672243ab588b2573c3115
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2024-10-03 11:11:52 -0600

    Origin story container complete

--- costa-rica-course/src/app/about/page.tsx
+++ costa-rica-course/src/app/about/page.tsx
@@ -24,10 +24,17 @@ const AboutPage = () => {
           <div className={styles.images}>
             <div className={styles.imageTop}>
               <div className={styles.imageTopLeft}></div>
-              <div className={styles.imageTopRight}></div>
+              <div className={styles.imageTopRight}>
+                <h4>Dr. John Smith</h4>
+                <h5>Founder & CEO</h5>
+              </div>
             </div>
             <div className={styles.imageBottom}>
-              <div className={styles.imageBottomLeft}></div>
+              <div className={styles.imageBottomLeft}>
+                <h4>Lisa Green</h4>
+                <h5>CMO</h5>
+
+              </div>
               <div className={styles.imageBottomRight}></div>
             </div>
           </div>
