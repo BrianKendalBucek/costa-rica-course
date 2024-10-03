190d35a53dbb4f40ae7d5c33eceb570b890ebc99
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2024-10-03 11:33:55 -0600

    stat bar complete in about page

--- costa-rica-course/src/app/about/page.tsx
+++ costa-rica-course/src/app/about/page.tsx
@@ -33,11 +33,28 @@ const AboutPage = () => {
               <div className={styles.imageBottomLeft}>
                 <h4>Lisa Green</h4>
                 <h5>CMO</h5>
-
               </div>
               <div className={styles.imageBottomRight}></div>
             </div>
           </div>
+        </div>
+      </div>
+      <div className={styles.statBarContainer}>
+        <div className={styles.statBarSection}>
+          <h2>10+</h2>
+          <p>Awards</p>
+        </div>
+        <div className={styles.statBarSection}>
+          <h2>5+</h2>
+          <p>Countries</p>
+        </div>
+        <div className={styles.statBarSection}>
+          <h2>12+</h2>
+          <p>Partners</p>
+        </div>
+        <div className={styles.statBarSection}>
+          <h2>7k</h2>
+          <p>Students</p>
         </div>
       </div>
     </div>
