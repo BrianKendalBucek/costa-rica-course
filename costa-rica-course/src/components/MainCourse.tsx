8cf5bce42b7bac7ebc0f8586c1cd58c0ecab5c73
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2024-10-08 14:47:13 -0600

    progress on main course container

--- costa-rica-course/src/components/MainCourse.tsx
+++ costa-rica-course/src/components/MainCourse.tsx
@@ -14,12 +14,14 @@ const MainCourse = () => {
         />
       </div>
       <div className={styles.mainCourseDetails}>
-        <h2>How to Move to Costa Rica!</h2>
-        <p>
-          Unlock the secrets to a successful expat life in Costa Rica with our
-          comprehensive course, covering everything from legal procedures to
-          cultural integration.
-        </p>
+        <div className={styles.headerAndParagraph}>
+          <h2>How to Move to Costa Rica!</h2>
+          <p>
+            Unlock the secrets to a successful expat life in Costa Rica with our
+            comprehensive course, covering everything from legal procedures to
+            cultural integration.
+          </p>
+        </div>
         <div className={styles.priceAndButton}>
           <div className={styles.priceOldAndNew}>
             <p className={styles.priceOld}>$499</p>
