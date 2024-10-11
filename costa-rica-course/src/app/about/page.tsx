fa00ffefd9bd6393036b6acbd0968182a6e64d62
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2024-10-11 13:57:32 -0600

    cleaned up about us page with components

--- costa-rica-course/src/app/about/page.tsx
+++ costa-rica-course/src/app/about/page.tsx
@@ -4,6 +4,8 @@ import { Instructor } from "@/components/Instructor";
 import { Students } from "@/components/Students";
 import { Stats } from "@/components/Stats";
 import { Hero } from "@/components/Hero";
+import OriginStory from "@/components/OriginStory";
+import Founders from "@/components/Founders";
 
 const AboutPage = () => {
   return (
@@ -17,50 +19,11 @@ const AboutPage = () => {
       />
       <div className={styles.storyContainer}>
         <div className={styles.storyAndImageContainer}>
-          <div className={styles.story}>
-            <h2>ORIGIN STORY</h2>
-            <p>
-              This course was born out of the personal journey of its creator, a
-              Canadian expat who made the life-changing decision to move to
-              Costa Rica several years ago. Faced with the challenges and
-              adventures of adapting to a new culture, navigating the legal
-              intricacies of residency, and learning the ropes of living in a
-              foreign land, they recognized a gap in available resources for
-              those looking to make a similar move. Leveraged by their firsthand
-              experiences, trials, and successes, the course is designed to
-              offer a roadmap to prospective expats. <br />
-              <br />
-              It encapsulates years of accumulated knowledge, insights, and
-              practical advice, aiming to smooth the path for others by
-              providing a comprehensive guide to relocating, settling, and
-              thriving in the beautiful and diverse country of Costa Rica. The
-              creator&apos;s passion for their adopted home and desire to share
-              the beauty of expat life with others is the driving force behind
-              this course, making it not just a set of instructions but a
-              personal guide infused with love and enthusiasm for Costa Rican
-              life.
-            </p>
-          </div>
-          <div className={styles.images}>
-            <div className={styles.imageTop}>
-              <div className={styles.imageTopLeft}></div>
-              <div className={styles.imageTopRight}>
-                <h4>Dr. John Smith</h4>
-                <h5>Founder & CEO</h5>
-              </div>
-            </div>
-            <div className={styles.imageBottom}>
-              <div className={styles.imageBottomLeft}>
-                <h4>Lisa Green</h4>
-                <h5>CMO</h5>
-              </div>
-              <div className={styles.imageBottomRight}></div>
-            </div>
-          </div>
+          <OriginStory />
+          <Founders />
         </div>
       </div>
       <Stats />
-      {/* <Instructor /> */}
       <div className={styles.instructorContainer}>
         <Instructor />
       </div>
---
+++ costa-rica-course/src/components/OriginStory.tsx
@@ -0,0 +1,30 @@
+import styles from "./OriginStory.module.scss";
+
+const OriginStory = () => {
+  return (
+    <div className={styles.story}>
+      <h2>ORIGIN STORY</h2>
+      <p>
+        This course was born out of the personal journey of its creator, a
+        Canadian expat who made the life-changing decision to move to Costa Rica
+        several years ago. Faced with the challenges and adventures of adapting
+        to a new culture, navigating the legal intricacies of residency, and
+        learning the ropes of living in a foreign land, they recognized a gap in
+        available resources for those looking to make a similar move. Leveraged
+        by their firsthand experiences, trials, and successes, the course is
+        designed to offer a roadmap to prospective expats. <br />
+        <br />
+        It encapsulates years of accumulated knowledge, insights, and practical
+        advice, aiming to smooth the path for others by providing a
+        comprehensive guide to relocating, settling, and thriving in the
+        beautiful and diverse country of Costa Rica. The creator&apos;s passion
+        for their adopted home and desire to share the beauty of expat life with
+        others is the driving force behind this course, making it not just a set
+        of instructions but a personal guide infused with love and enthusiasm
+        for Costa Rican life.
+      </p>
+    </div>
+  );
+};
+
+export default OriginStory;
