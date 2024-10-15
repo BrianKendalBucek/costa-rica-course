af2104a29953d19e8b48c84f557a7f3260813301
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2024-10-15 12:27:28 -0600

    Footer responsive

--- costa-rica-course/src/components/Footer.tsx
+++ costa-rica-course/src/components/Footer.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
 // import Link from 'next/link';
-import styles from './Footer.module.scss';
+import styles from "./Footer.module.scss";
 // import Image from 'next/image';
 
 const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
-      <h1>Join our happy students today!</h1>
+      <div className={styles.headerDivison}>
+        <h1>Join our happy</h1>
+        <h1 className={styles.titleTwo}>students today!</h1>
+      </div>
       <p>Begin your journey to a new life in Costa Rica</p>
       <button>Begin The Adventure</button>
       <p className={styles.copyright}>Copyright ® 2024 The Costa Rica Course</p>
