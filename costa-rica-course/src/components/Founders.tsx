2891b3b0a20149fd0ea19fea74b8bf209dbf8480
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2024-10-16 15:11:45 -0600

    improved responsiveness of about page founders data

--- costa-rica-course/src/components/Founders.tsx
+++ costa-rica-course/src/components/Founders.tsx
@@ -2,20 +2,20 @@ import styles from "./Founders.module.scss";
 
 const Founders = () => {
   return (
-    <div className={styles.images}>
-      <div className={styles.imageTop}>
-        <div className={styles.imageTopLeft}></div>
-        <div className={styles.imageTopRight}>
-          <h4>Dr. John Smith</h4>
-          <h5>Founder & CEO</h5>
-        </div>
-      </div>
-      <div className={styles.imageBottom}>
-        <div className={styles.imageBottomLeft}>
+    <div className={styles.foundersImagesAndData}>
+      <div className={styles.founderLeft}>
+        <div className={styles.image}></div>
+        <div className={styles.data}>
           <h4>Lisa Green</h4>
           <h5>CMO</h5>
         </div>
-        <div className={styles.imageBottomRight}></div>
+      </div>
+      <div className={styles.founderRight}>
+        <div className={styles.image}></div>
+        <div className={styles.data}>
+          <h4>Dr. John Smith</h4>
+          <h5>Founder & CEO</h5>
+        </div>
       </div>
     </div>
   );
